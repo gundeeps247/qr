da6c3980a0b7b5ef927050a9810fdc24c1f7c051
Gundeep Singh <gundeepsinghm@gmail.com>
2024-01-09 18:51:55 +0530

    c

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    # Return an empty response or a placeholder image
+    return ''
+
 @app.route('/')
 def index():
     return render_template('index.html')
