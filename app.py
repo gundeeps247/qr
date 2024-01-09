e84fa143acc37facb2ff24fce3627c8bbcbf8c39
Gundeep Singh <gundeepsinghm@gmail.com>
2024-01-09 18:35:49 +0530

    c

---
+++ app.py
@@ -0,0 +1,22 @@
+import qrcode
+from flask import Flask, render_template, request, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate_qrcode', methods=['POST'])
+def generate_qrcode():
+    name = request.form['name']
+    age = int(request.form['age'])
+
+    data = f"Name: {name}\nAge: {age}"
+    img = qrcode.make(data)
+    img.save("myqr.png")
+
+    return send_file("myqr.png", mimetype='image/png', as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
